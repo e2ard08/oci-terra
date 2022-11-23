e49b9fd686ec2e9edcd060cadc09ab6fc3ac9a8f
e2ard <edward.8805@gmail.com>
2022-11-22 22:47:06 -0500

    first commit

---
+++ variable.tf
@@ -0,0 +1,7 @@
+compartment_id   = "ocid1.tenancy.oc1..aaaaaaaanfp6fmyby7e35a6okh7hd6r5zldobzstedxmxxszc7ngiax65osq"
+compartment_name = terraform
+tenancy_ocid     = "ocid1.tenancy.oc1..aaaaaaaanfp6fmyby7e35a6okh7hd6r5zldobzstedxmxxszc7ngiax65osq"
+user_ocid        = "ocid1.user.oc1..aaaaaaaar765z53p2qff26vykqkmqyr74ynrxfalcxbebcjkq33qacjfvwnq"
+fingerprint      = "0f:4a:73:25:f0:2c:8e:d8:ef:5f:79:71:36:03:e5:13"
+private_key      = "/Users/e2ard/.oci/oci_api_key_private.pem"
+region           = us-ashburn-1
